0e85222108dc1b19b32447b7a4c7083a1d7a1100
Usong Cho <silvermouse4012@gmail.com>
2022-02-06 19:39:34 -0800

    inplemented the 2 digit limit on the input field

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ const startGuessing = document.querySelector('.start-guessing');
 let randomNum = Math.round(Math.random() * 20) + 1;
 console.log(randomNum);
 
-input.addEventListener('keypress', () => {
-	console.log(input.value);
-	if (input.value.length > 1) {
-		input.value = input.value.slice(0, 2);
+input.addEventListener('keypress', e => {
+	if (e.key === 'Enter') {
+		return null;
+	} else if (input.value.length >= 0) {
+		input.value = input.value.slice(0, 1);
+		console.log(input.value.length);
 	}
 });
 
 const correctNum = () => {
-	console.log(input.value);
 	let int = parseInt(input.value);
 
 	if (randomNum > int) {
